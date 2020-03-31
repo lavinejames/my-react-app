ab1a7d4792e80c503e2ee60ed7cef63794a57095
lavinejames <hello@jameslavine.com>
2020-03-30 20:13:38 -0400

    changes up to lesson 10 of react for designers course

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: "James' React Site",
+    title: 'Design + Code',
+    description: 'Complete courses about the best tools and design systems.',
+    keywords: 'react course, react for designers'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-react-helmet'
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'o6lude3oehs2',
+        accessToken: 'eWk5uRwuEiuqFeyhHOzCks6jwxz3iCyou38YeRINRYg'
+      }
+    }
+  ],
 }
